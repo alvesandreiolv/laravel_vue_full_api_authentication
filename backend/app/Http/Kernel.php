9052957009bfd6874e1af307bb9db99313faa4a7
Neyelson Alves <neyelson@gmail.com>
2023-11-16 21:07:43 -0300

    adding route for retrieving CSFR token 3

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
