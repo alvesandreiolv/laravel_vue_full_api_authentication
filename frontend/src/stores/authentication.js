d702218e83dce01353eb8b8030c59bbcd3e4cff2
Neyelson Alves <neyelson@gmail.com>
2023-11-19 21:39:29 -0300

    implementing authentication with pinia 7

--- frontend/src/stores/authentication.js
+++ frontend/src/stores/authentication.js
@@ -13,7 +13,6 @@ export const useAuthStore = defineStore('auth', {
         const response = await axios.post('https://evbkzynoncxd.neptune.trulywired.link/api/login', {
           username,
           password,
-          headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
         });
       } catch (error) {
         // Handle login error (e.g., display error message)
