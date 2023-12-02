c6d72f34b125d83adb4afbe2f729c9ff95a7ac82
Neyelson Alves <neyelson@gmail.com>
2023-12-02 16:57:49 -0300

    implementing change username and password backend functionalities 2

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
 
     public function changeUsername(Request $request)
     {
+        // Do request validation
+        if (Validator::make($request->all(), [
+            'password' => 'required',
+            'newUsername' => 'required',
+        ])->fails()) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
+        }
+
+        // Check if password is correct.
+        if (!Auth::attempt(['email' => auth()->user()->email, 'password' => $request->password])) {
+            return response()->json(['message' => 'Password is incorrect.', 'errors' => $validator->errors()], 401);
+        }
+
         return 'You would change username.';
     }
 
