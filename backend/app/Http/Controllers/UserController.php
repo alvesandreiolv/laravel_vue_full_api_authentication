0144b0ce0ecd42f343420ad5d439fe6e95f8d5c4
Neyelson Alves <neyelson@gmail.com>
2023-12-02 18:57:28 -0300

    implementing change username and password backend functionalities 8

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
         // Check if validation fails and return errors.
         if ($validator->fails()) {
-            return response()->json(['message' => 'Validation failed.', 'errors' => $validator->errors()], 422);
+            return response()->json(['message' => 'Validation failed.', 'errors' => $validator->errors()->all()], 422);
         }
 
         // Check if password is correct.
