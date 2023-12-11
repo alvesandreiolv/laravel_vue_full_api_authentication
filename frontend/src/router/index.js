8babd9456e7c51028608256a9cf857e2ed00f30e
neyelson <neyelson@gmail.com>
2023-12-11 15:03:14 -0300

    adding page titles 1

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,36 +4,43 @@ const routes = [
   {
     path: "/",
     name: "Public Page",
+    meta: { title: 'Public' },
     component: () => import("../views/PublicPage.vue"),
   },
   {
     path: "/login",
     name: "Login",
+    meta: { title: 'Login' },
     component: () => import("../views/Login.vue"),
   },
   {
     path: "/signup",
     name: "SignUp Page",
+    meta: { title: 'Sign Up' },
     component: () => import("../views/SignUp.vue"),
   },
   //Below are all internal dashboard pages
   {
     path: "/home",
     name: "Home",
+    meta: { title: 'Home' },
     component: () => import("../views/dashboardViews/MainDashboard.vue"),
     children: [
       {
         path: "",
+        meta: { title: 'Home' },
         component: () => import("../views/dashboardViews/Home.vue"),
       },
       {
         name: "My Info",
         path: "/myinfo",
+        meta: { title: 'My Info' },
         component: () => import("../views/dashboardViews/MyInfo.vue"),
       },
       {
         name: "Settings",
         path: "/settings",
+        meta: { title: 'Settings' },
         children: [
           {
             path: "",
@@ -43,6 +50,7 @@ const routes = [
           {
             name: "Change Password",
             path: "changepassword",
+            meta: { title: 'Change Password' },
             component: () =>
               import(
                 "../views/dashboardViews/settingsViews/ChangePassword.vue"
@@ -51,6 +59,7 @@ const routes = [
           {
             name: "Change Name",
             path: "changename",
+            meta: { title: 'Change Display Name' },
             component: () =>
               import(
                 "../views/dashboardViews/settingsViews/ChangeName.vue"
@@ -65,16 +74,20 @@ const routes = [
   {
     path: "/:pathMatch(.*)",
     name: "Not Found",
+    meta: { title: 'Not Found' },
     component: () => import("../views/NotFound.vue"),
   },
 ];
-
+// Below is system related config
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
+// Below is all related to what before next page is loaded.
 router.beforeEach((to, from, next) => {
+
+  /* Below is redirection log bloc */
   const authToken = localStorage.hasOwnProperty(import.meta.env.VITE_AUTHTOKEN_NAME);
   // If the user is authenticated and trying to access the login page, redirect to the dashboard home
   if (to.name === 'Login' && authToken) {
@@ -86,10 +99,15 @@ router.beforeEach((to, from, next) => {
     // Allow the user to continue
     next();
   }
+  /* End of redirection log bloc */
+
+  // Below adds page title if available
+  document.title = to.meta?.title ?? '';
+
 });
 
-router.afterEach((to, from) => {
-  //Adds loading stop
-});
+//Below is log after page loads, in case is necessary.
+//router.afterEach((to, from) => {
+//});
 
 export default router; // export the router instance
